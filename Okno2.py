367e8fd7bb0a4c080a878f9b001c1dc5dd0a4182
Michasx Black <Michasx.malpa@gmail.com>
2021-08-02 22:01:50 +0200

    System pokazujący błąd (brak wyszukiwanej mangi) - włącza się tylko raz

--- Okno2.py
+++ Okno2.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from Błąd import Error
 import sqlite3
 
 t = sqlite3.connect('database.db')
 mycursor = t.cursor()
 
+test = 0
 var1 = 0
 var2 = 0
 
@@ -13,6 +15,7 @@ class Dodawanie():
         #self.root1 = root1
 
     def Ustawienia(self):
+        self.test = test
         self.root1.title("Mangi")
         self.root1.config(bg = "#1B1324")
         self.root1.resizable(width=False, height=False)
@@ -46,21 +49,32 @@ class Dodawanie():
         self.root1.mainloop()
 
     def Dodawanie(self):
-        print('a')
         var1 = str(self.get_1.get())
-        var2 = int(self.get_2.get())
         #Lista otrzymuje 1 tom
-        mycursor.execute(''' UPDATE `Mangi` SET `Ilość` = Ilość + 1 WHERE `Nazwa_Mangi` = ? ''', [var1])
-        #cena całkowita jest powiększana o dany tom
-        mycursor.execute('''SELECT `Cena` FROM `mangi` WHERE `Nazwa_Mangi` = ?''',[var1])
-        result1 = [item[0] for item in mycursor.fetchall()]
-        if result1[0] > 5:
-            mycursor.execute(''' UPDATE `Mangi` SET `Cała_Cena` = Cała_Cena + ? WHERE `Nazwa_Mangi` = ? ''', [result1[0],var1])
-        #dodano tom do 'Od_do'
-        mycursor.execute('''SELECT `Do` FROM `Mangi` WHERE `Nazwa_Mangi` = ?''', [var1])
-        result2 = [item[0] for item in mycursor.fetchall()]
-        if result2[0] > var2:
-            mycursor.execute(''' UPDATE `Mangi` SET `Od` = ? WHERE `Nazwa_Mangi` = ? ''', [var2,var1])
-        else:
-            mycursor.execute(''' UPDATE `Mangi` SET `Do` = ? WHERE `Nazwa_Mangi` = ? ''', [var2,var1])
-        t.commit()
+        mycursor.execute('''SELECT `Nazwa_Mangi` FROM `mangi`''')
+        result = [item[0] for item in mycursor.fetchall()]
+        for i in result:
+            if var1 == i:
+                var2 = int(self.get_2.get())
+                mycursor.execute(''' UPDATE `Mangi` SET `Ilość` = Ilość + 1 WHERE `Nazwa_Mangi` = ? ''', [var1])
+                #cena całkowita jest powiększana o dany tom
+                mycursor.execute('''SELECT `Cena` FROM `mangi` WHERE `Nazwa_Mangi` = ?''',[var1])
+                result1 = [item[0] for item in mycursor.fetchall()]
+                if result1[0] > 5:
+                    mycursor.execute(''' UPDATE `Mangi` SET `Cała_Cena` = Cała_Cena + ? WHERE `Nazwa_Mangi` = ? ''', [result1[0],var1])
+                #dodano tom do 'Od_do'
+                mycursor.execute('''SELECT `Do` FROM `Mangi` WHERE `Nazwa_Mangi` = ?''', [var1])
+                result2 = [item[0] for item in mycursor.fetchall()]
+                if result2[0] > var2:
+                    mycursor.execute(''' UPDATE `Mangi` SET `Od` = ? WHERE `Nazwa_Mangi` = ? ''', [var2,var1])
+                else:
+                    mycursor.execute(''' UPDATE `Mangi` SET `Do` = ? WHERE `Nazwa_Mangi` = ? ''', [var2,var1])
+                t.commit()
+            else:
+                self.test=self.test+1
+        if self.test == len(result):
+            error = Error()
+            error.root3 = Tk()
+            error.Ustawienia()
+            error.Okno()
+            error.Start()
